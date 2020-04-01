c23789ce1e5c02be68f51ebea11eaa4b9666b182
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-31 18:11:32 -0700

    Initialize CretaceousClient, add GetAll() function with MVC

---
+++ CretaceousClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CretaceousClient
+{
+    public class Program
+    {
+    public static void Main(string[] args)
+    {
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
+    }
+
+    // class ApiHelper
+    // {
+    //     public static async Task<string> ApiCall(string apiKey)
+    //     {
+    //     RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+    //     RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+    //     var response = await client.ExecuteTaskAsync(request);
+    //     return response.Content;
+    //     }
+    // }
+}
